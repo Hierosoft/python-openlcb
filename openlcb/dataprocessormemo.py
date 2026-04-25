1682c765c40774fbdae343ac371b13cebd6e2dbe
Jacob Gustafson <7557867+Poikilos@users.noreply.github.com>
2026-04-24 20:04:05 -0400

    Make a DataProcessorMemo superclass for parsing messages that are not XML nodes.

---
+++ openlcb/dataprocessormemo.py
@@ -0,0 +1,32 @@
+from typing import Union
+
+from openlcb.message import Message
+
+
+class DataProcessorMemo:
+    """Store parsing state info.
+    This superclass can be used for progress notification.
+
+    Attributes:
+        done (bool): If True, download such as downloadCDI is finished.
+            Though document itself may be incomplete if 'error' is also
+            set, stop tracking status of download regardless.
+        end (bool): False to start a deeper scope, or True for end tag,
+            which exits current scope (last created Treeview branch in
+            this case, or top if getBranch() would be None).
+        error (str): Message of failure (requires 'done' if stopped).
+        message (Message): Associated network/internal message.
+        name (str): Name (determined by `name` child element content).
+        status (str): Status message.
+    """
+    def __init__(self, status: Union[str, None] = None):
+        self.done = False  # type: bool
+        self.end = False  # type: bool
+        self.error = None  # type: str|None
+        self.message: Union[Message, None] = None  # type: Message|None
+        self.status = status   # type: str|None
+        # region set by DataProcessor such as XMLDataProcessor
+        self.progress_ratio = None  # type: float|None
+        self.progress_count = None  # type: int|None
+        self.expected_size = None  # type: int|None
+        # end region set by DataProcessor such as XMLDataProcessor
